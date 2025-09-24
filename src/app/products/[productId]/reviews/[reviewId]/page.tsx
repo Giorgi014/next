8b4f6664ce0c19241b5e907e32c17fd47322c737
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-09-24 21:37:48 +0400

    first commit

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,17 @@
+import { notFound } from "next/navigation";
+
+export default function ReciewDetails({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) {
+  if (parseInt(params.reviewId > 1000)) {
+    notFound();
+  }
+
+  return (
+    <h1>
+      Reviewss {params.reviewId} for products {params.productId}
+    </h1>
+  );
+}
