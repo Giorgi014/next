ae857e28742073495bb3a683437775e44e282476
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-01 02:20:03 +0400

    project update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,13 @@
-export const metaData = {
-  title: "About Codevolution",
-};
+// export const metaData = {
+//   title: "About Codevolution",
+// };
 
-export default function About() {
-  return <h1>About me</h1>;
+
+// export default function About() {
+//   return <h1>About me</h1>;
+// }
+
+export default function AboutPage() {
+  console.log("about me");
+  return <h1>About Page</h1>;
 }
